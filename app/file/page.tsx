d3214ec67ab61f231f9bd43bf1ec752f06d0b396
DanielWTE <daniel.iuser@icloud.com>
2023-06-27 09:51:14 +0200

    ui fix

--- app/file/page.tsx
+++ app/file/page.tsx
@@ -270,10 +270,11 @@ function UploadFile() {
                   <button
                     id="submit"
                     disabled={isUploading}
-                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold px-6 py-3 rounded transition duration-200"
+                    className="bg-blue-500 hover:bg-blue-600 text-center text-white font-bold px-6 py-3 rounded transition duration-200"
+                    style={{ minWidth: '110px' }}
                   >
                     {isUploading ? (
-                      <div className="flex items-center">
+                      <div className="flex items-center text-center">
                         <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                           <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                           <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
